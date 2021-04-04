e4a74e60f5a4e08239096d282976432f5be1a08c
Victor Teka <ambukateka@gmail.com>
2021-04-05 00:08:05 +0300

    Create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header__container'>
+            <h1>Welcome to sports trivia app</h1>
+        </div>
+    )
+}
+
+export default Header
